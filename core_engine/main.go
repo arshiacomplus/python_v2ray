c42d01c6182c638bbccc60dbbc69dfc40840d64d
Arshia <163334604+arshiacomplus@users.noreply.github.com>
2025-09-01 15:53:40 +0330

    Add files via upload

--- core_engine/main.go
+++ core_engine/main.go
@@ -123,7 +123,7 @@ func main() {
 }
 
 func testProxy(port int) (int64, string) {
-	targetURL := "http://cp.cloudflare.com/generate_204"
+	targetURL := "http://www.google.com/generate_204"
 	timeout := 8 * time.Second
 
 	dialer, err := proxy.SOCKS5("tcp", fmt.Sprintf("127.0.0.1:%d", port), nil, proxy.Direct)
